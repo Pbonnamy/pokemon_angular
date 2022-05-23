30dd52e9799773b10da6273502993cb7d10668c2
Pbonnamy <bonnamy.77@gmail.com>
2022-05-23 10:57:32 +0200

    pokemon pv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-pokemon';
 
   pokemons = [
     {
-      img: "006"
+      img: '006',
+      pv: 250,
+      currentPv: 125,
     },
     {
-      img: "009"
-    }
-  ]
+      img: '009',
+      pv: 100,
+      currentPv: 25,
+    },
+  ];
 }
