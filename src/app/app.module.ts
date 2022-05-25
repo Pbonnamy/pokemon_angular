f08e764bee18329534e03cc1e289a5435e0e1252
Pbonnamy <bonnamy.77@gmail.com>
2022-05-25 14:29:02 +0200

    pages / components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatGridListModule} from "@angular/material/grid-list";
-import { PokemonStatsComponent } from './pokemon-stats/pokemon-stats/pokemon-stats.component';
+import { PokemonStatsComponent } from './components/pokemon-stats/pokemon-stats.component';
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatCardModule} from "@angular/material/card";
 
